12990d28fc5eda75156dc8a1ecdc2d3f22fb82e6
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-17 13:02:05 +0700

    lagi commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todo.js');
 const port = 3000;
-
+app.use(express.json());
 app.use('/todos', todoRoutes);
 app.set('view engine','ejs');
 
