f53bb0db8b87b59ddc7c90372155137e68c0a46d
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-29 19:39:16 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -23,7 +23,30 @@ router.post('/', (req, res) => {
         id: todos.length +1, 
         task: req.body.task, 
         completed: false 
-    } 
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
 }); 
  
 module.exports = router;
+
+// Endpoint untuk menghapus tugas
+router.delete('/:id',(req, res)=> {
+    const todoIndex = todos.findIndex (t => t.id === parseInt(req.params.id));
+    if(todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan'});
+
+    const deletedTodo = todos.splice(todoIndex, 1)[0];
+    res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
+} );
+
+// Endpoint untuk memperbarui tugas
+router.put('/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json ({message: 'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+    
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbaharui`,
+        updateTodo: todo
+    });
+});
