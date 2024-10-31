4e2707ba1b7d453a49e9d4e24b1bef665b3716fc
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-31 12:54:21 +0700

    pertemuan31/otkt/24

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const todoRoutes = require('./routes/tododb.js');
 const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 3000;
-app.use(express.json());
-app.use('/todos', todoRoutes);
-app.set('view engine','ejs');
+require('dotenv').config();
+const port = process.env.PORT;
 
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
 });
 
 app.listen(port, () => {
